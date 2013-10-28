57db47fa86f584979006e5d6bca436d5df9ef0f0
circularuins <ubuntu11@circularuins.(none)>
2013-10-28 18:36:01 +0900

    大掃除した。特にpackage/archivesの扱いには注意。ubuntu以外ではpackageをインストールしない。

---
+++ conf/02-edit.el
@@ -0,0 +1,272 @@
+;;;;;;;;;;;;;;;;;;;;
+;;; カーソル関連 ;;;
+;;;;;;;;;;;;;;;;;;;;
+
+;;; #削除系コマンド ;;;
+
+;; C-hで直前の文字を消去
+(global-set-key (kbd "C-h") 'delete-backward-char)
+
+;; C-x C-hでhelp
+(global-set-key (kbd "C-x C-h") 'help)
+
+;; C-k(kill-line)で行末の改行までkill
+(setq kill-whole-line 1)
+
+;; カーソル位置から行頭まで削除する
+(defun backward-kill-line (arg)
+  "Kill chars backward until encountering the end of a line."
+  (interactive "p")
+  (kill-line 0))
+;; C-M-kに設定
+(global-set-key (kbd "C-M-k") 'backward-kill-line)
+
+;; 範囲指定していないとき、C-wで前の単語を削除
+(defadvice kill-region (around kill-word-or-kill-region activate)
+  (if (and (interactive-p) transient-mark-mode (not mark-active))
+      (backward-kill-word 1)
+    ad-do-it))
+;; minibuffer用
+(define-key minibuffer-local-completion-map "\C-w" 'backward-kill-word)
+
+;; カーソル位置の単語を削除
+(defun kill-word-at-point ()
+  (interactive)
+  (let ((char (char-to-string (char-after (point)))))
+    (cond
+     ((string= " " char) (delete-horizontal-space))
+     ((string-match "[\t\n -@\[-`{-~]" char) (kill-word 1))
+     (t (forward-char) (backward-word) (kill-word 1)))))
+(global-set-key "\M-d" 'kill-word-at-point)
+
+;;; #移動系コマンド ;;;
+
+;; ウィンドウ内のカーソル移動
+(global-set-key (kbd "C-M-h") (lambda () (interactive) (move-to-window-line 0))) ; 画面上に移動
+(global-set-key (kbd "C-M-m") (lambda () (interactive) (move-to-window-line nil))) ; 画面中に移動
+(global-set-key (kbd "C-M-l") (lambda () (interactive) (move-to-window-line -1))) ; 画面下に移動
+
+;; 最後の変更箇所にジャンプする
+; M-x install-elisp-from-emacswiki goto-chg.el
+(require 'goto-chg)
+(define-key global-map (kbd "<f8>") 'goto-last-change)
+(define-key global-map (kbd "S-<f8>") 'goto-last-change-reverse)
+
+;; カーソル位置を戻す
+(require 'point-undo)
+; M-x install-elisp-from-emacswiki point-undo.el
+(define-key global-map (kbd "<f7>") 'point-undo)
+(define-key global-map (kbd "S-<f7>") 'point-redo)
+
+;;; #表示 ;;;
+
+;; 相対的なカーソル位置を保ったまま画面スクロール
+(setq scroll-preserve-screen-position t)
+
+;;; カーソル位置に目に見える印をつける
+;; (setq-default bm-buffer-persistence nil)
+;; (setq bm-restore-repository-on-load t)
+;; (require 'bm)
+;; (add-hook 'find-file-hooks 'bm-buffer-restore)
+;; (add-hook 'kill-buffer-hook 'bm-buffer-save)
+;; (add-hook 'after-save-hook 'bm-buffer-save)
+;; (add-hook 'after-revert-hook 'bm-buffer-restore)
+;; (add-hook 'vc-before-checkin-hook 'bm-buffer-save)
+;; (global-set-key (kbd "M-SPC") 'bm-toggle)
+;; (global-set-key (kbd "M-[") 'bm-previous)
+;; (global-set-key (kbd "M-]") 'bm-next)
+
+;;; 現在行・桁のハイライト
+;; (require 'crosshairs)
+;; (global-set-key (kbd "C-c h") 'crosshairs-mode)
+
+;;; #その他 ;;;
+
+;; キーボード同時押しコマンドの設定
+;; (require 'key-chord)
+;; (setq key-chord-two-keys-delay 0.04)
+;; (key-chord-mode 1)
+
+;;; 同じキーバインドを連打すると警告を出す
+;; (require 'dont-type-twice)
+;; (global-dont-type-twice t)
+
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;
+;;; 編集系の設定 ;;;
+;;;;;;;;;;;;;;;;;;;;
+
+;;; 矩形編集 ;;;
+
+;; cua-mode
+;; C-RETで開始、C-gで終了
+;; #連番入力の手順
+;; 矩形選択後、M-oでスペース1文字挿入
+;; M-n 後、初期値、加算値、フォーマットの順に入力
+(cua-mode t)
+(setq cua-enable-cua-keys nil) ; CUAキーバインドを無効に
+; ターミナルでデフォルトの"C-RET"が使えないので変更する
+(define-key global-map (kbd "C-x C-x") 'cua-set-rectangle-mark)
+
+;;; grep系 ;;;
+
+;; M-x grep 検索結果を編集可能に
+;; ack --nogroup hoge でackが使える
+; M-x install-elisp-from-emacswiki grep-edit.el
+(require 'grep-edit)
+
+;;; その他;;;
+
+;; tailモード
+; M-x install-elisp-from-emacswiki tail.el
+(require 'tail)
+(setq tail-volatile nil)
+(setq tail-hide-delay 100000)
+(setq tail-max-size 15)
+
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;
+;;; ファイル操作 ;;;
+;;;;;;;;;;;;;;;;;;;;
+
+;; バックアップファイルを作らない
+(setq make-backup-files nil)
+
+;; オートセーブファイルを作らない
+;(setq auto-save-default nil)
+
+;; オートセーブファイルを~/.emacs.d/backups/へ集める
+;; M-x recover-file RET ~/.emacs.d/backups/hoge.hoge RET でバッファが復元される
+(setq auto-save-file-name-transforms
+      `((".*" ,(expand-file-name "~/.emacs.d/backups/") t)))
+
+;; オートセーブファイル作成までの時間とタイプ間隔
+(setq auto-save-timeout 15)
+(setq auto-save-interval 60)
+
+;; 終了時にオートセーブファイルを消す
+(setq delete-auto-save-files t)
+
+;; 最近開いたファイルを保存する数を増やす
+(setq recentf-max-saved-items 10000)
+
+;; 最近使ったファイルを開く
+; M-x install-elisp-from-emacswiki recentf-ext.el
+(setq recentf-exclude '("/TAGS$" "/var/tmp/" "Map_Sym.txt")) ; リストからの除外対象
+(require 'recentf-ext)
+(define-key global-map (kbd "C-x C-m") 'recentf-open-files)
+
+;; diredを便利にする
+(require 'dired-x)
+
+;; diredから"r"でファイル名をインライン編集する
+(require 'wdired)
+(define-key dired-mode-map "r" 'wdired-change-to-wdired-mode)
+
+;; wdiredで、ファイルのパーミションを編集可能にする
+;; "C-x d"でwdired、"C-x C-q"で編集モード
+(setq wdired-allow-to-change-permissions t)
+
+;; ファイル名が重複していたらディレクトリ名を追加する。
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
+
+;; 現在カーソル位置のファイル・URLを開く
+(ffap-bindings)
+
+
+
+
+
+;;;;;;;;;;;;;;;;
+;;; 履歴操作 ;;;
+;;;;;;;;;;;;;;;;
+
+;;; #undo,redo ;;;
+
+;; undoの分岐を視覚化(C-x u)
+;; "p","n"で履歴を移動、"f","b"でブランチの切り替え、"q"で終了
+; packageでインストール
+(require 'undo-tree)
+(global-undo-tree-mode t)
+(global-set-key (kbd "M-/") 'undo-tree-redo)
+
+;;; #履歴の保存 ;;;
+
+;; 履歴を次回emacs起動畤にも保存する
+(savehist-mode 1)
+
+;; 履歴数を大きくする
+(setq history-length t)
+
+;; kill-ringやミニバッファで過去に開いたファイルなどの履歴を保存する
+; install-elisp-from-emacswiki session.el
+(when (require 'session nil t)
+  (setq session-initialize '(de-saveplace session keys menus places)
+        session-globals-include '((kill-ring 50)
+                                  (session-file-alist 500 t)
+                                  (file-name-history 10000)))
+  (add-hook 'after-init-hook 'session-initialize)
+  (setq session-undo-check -1)) ; 前回閉じたときの位置にカーソルを復帰
+
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;
+;;; ルート権限で編集 ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; ;; サーバーemacsclientとして開く
+;; ;; ~/bashrcに export EDITOR=emacsclient export VISUAL=emacsclient を追加しておく
+;; (server-start)
+;; (defun iconify-emacs-when-server-is-done ()
+;;   (unless server-clients (iconify-frame)))
+;; ;; 編集が終了したらEmacsをアイコン化する
+;; (add-hook 'server-done-hook 'iconify-emacs-when-server-is-done)
+;; ;; C-x C-cに割り当てる
+;; (global-set-key (kbd "C-x C-c") 'server-edit)
+;; ;; M-x exitでemacsを終了出来るようにする
+;; (defalias 'exit 'save-buffers-kill-emacs)
+
+;; ;; sudoを使う
+;; (server-start)
+;; (require 'sudo-ext)
+
+
+
+
+
+;;;;;;;;;;;;;;;;
+;;; メモ管理 ;;;
+;;;;;;;;;;;;;;;;
+
+;; org-mode
+;; Emacsでメモ・TODO管理
+(require 'org-install)
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(define-key global-map "\C-cr" 'org-remember)
+(setq org-startup-truncated nil)
+(setq org-return-follows-link t)
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(org-remember-insinuate)
+(setq org-directory "~/memo/")
+(setq org-default-notes-file (concat org-directory "notes.org"))
+(setq org-agenda-files '("~/memo/notes.org"))
+(setq org-remember-templates
+      '(("Todo" ?t "** TODO %?\n   %i\n   %a\n   %t" nil "Inbox")
+        ("Bug" ?b "** TODO %?   :bug:\n   %i\n   %a\n   %t" nil "Inbox")
+        ("Idea" ?i "** %?\n   %i\n   %a\n   %t" nil "New Ideas")))
+
+;; 試行錯誤用ファイル
+; M-x install-elisp-from-emacswiki open-junk-file.el
+(require 'open-junk-file)
+(setq open-junk-file-format "~/junk/%Y-%m-%d-%H%M%S.")
+(global-set-key (kbd "C-x C-z") 'open-junk-file)
---
+++ conf/04-programming.el
@@ -0,0 +1,323 @@
+;;;;;;;;;;;;
+;;; 全般 ;;;
+;;;;;;;;;;;;
+
+;; 実行権限の自動付与
+;; ファイルが #! から始まる場合、+X を付けて保存する
+(add-hook 'after-save-hook
+          'executable-make-buffer-file-executable-if-script-p)
+
+;;; ヘルプを綺麗に表示する
+; packageでインストール
+(require 'pos-tip)
+(defun my-describe-function (function)
+   "Display the full documentation of FUNCTION (a symbol) in tooltip."
+   (interactive (list (function-called-at-point)))
+   (if (null function)
+       (pos-tip-show
+        "** You didn't specify a function! **" '("red"))
+     (pos-tip-show
+      (with-temp-buffer
+        (let ((standard-output (current-buffer))
+              (help-xref-following t))
+          (prin1 function)
+          (princ " is ")
+          (describe-function-1 function)
+          (buffer-string)))
+      nil nil nil 0)))
+ (define-key emacs-lisp-mode-map (kbd "C-;") 'my-describe-function)
+
+;;; マニュアルビューアの設定 ;;;
+
+;; womanキャッシュを作成
+;; C-u M-x woman RET で更新
+(setq woman-cache-filename "~/.emacs.d/.wmncache.el")
+
+;; womanにmanパスを設定
+(setq woman-manpath '("/usr/share/man"
+                      "/usr/local/share/man"
+;                      "/usr/local/share/man/ja"))
+                      "/usr/local/share/man/jp"))
+
+;; ;; anything-for-document用のソースを定義
+;; ;; M-x anything-for-documentでドキュメントを串刺し検索
+;; (require 'anything)
+;; (setq anything-for-document-sources
+;;       (list anything-c-source-man-pages
+;;             anything-c-source-info-cl
+;;             anything-c-source-info-pages
+;;             anything-c-source-info-elisp
+;; ;            anything-c-source-info-emacs
+;; ;            anything-c-source-emacs-commands
+;;             anything-c-source-emacs-functions
+;;             anything-c-source-emacs-variables))
+
+;; ;; anything-for-documentコマンドを作成
+;; (defun anything-for-document ()
+;;   "Preconfigured `anything' for anything-for-document."
+;;   (interactive)
+;;   (anything anything-for-document-sources
+;;  ;           (thing-at-point 'symbol) nil nil nil
+;;             nil nil nil nil
+;;             "*anything for document*"))
+
+;;; ソースコード上でのプログラムの実行
+
+;; quickrunの設定
+; packageでインストール
+(require 'quickrun)
+;; popwinに追加
+(push '("*quickrun*" :position right) popwin:special-display-config)
+;; 実行結果がうまく表示されない対処療法
+(defadvice quickrun/apply-outputter (after quickrun/fix-scroll-buffer activate)
+  (recenter))
+;; region選択されていたらquickrun-region、されていなかったらquickrunを実行
+(defun quickrun-sc (start end)
+  (interactive "r")
+  (if mark-active
+      (quickrun :start start :end end)
+    (quickrun)))
+
+
+
+
+
+;;;;;;;;;;;;;;;
+;;; flymake ;;;
+;;;;;;;;;;;;;;;
+
+;; flymakeのハイライトをunderlineに変更
+;; ハイライトだとCUIで文字が見えないので、、
+(unless window-system
+  (custom-set-faces
+ '(flymake-errline ((((class color)) (:underline "red"))))
+ '(flymake-warnline ((((class color)) (:underline "yellow"))))))
+
+
+
+
+
+;;;;;;;;;;;;
+;;; Perl ;;;
+;;;;;;;;;;;;
+
+;; cperl-mode
+(defalias 'perl-mode 'cperl-mode)
+(add-to-list 'auto-mode-alist '("\\.psgi$" . cperl-mode))
+(add-to-list 'auto-mode-alist '("\\.t\\'"  . cperl-mode))
+(eval-after-load "cperl-mode"
+  '(progn
+     (cperl-set-style "PerlStyle")
+     (define-key cperl-mode-map (kbd "C-m") 'newline-and-indent)
+     (define-key cperl-mode-map (kbd "(") nil)
+     (define-key cperl-mode-map (kbd "{") nil)
+     (define-key cperl-mode-map (kbd "[") nil)
+     (define-key cperl-mode-map (kbd "M-n") 'flymake-goto-next-error)
+     (define-key cperl-mode-map (kbd "M-p") 'flymake-goto-prev-error)))
+(custom-set-variables
+ '(cperl-indent-parens-as-block t)
+ '(cperl-close-paren-offset     -4))
+
+;; perl-completion
+;; (auto-install-from-emacswiki "perl-completion.el")
+(autoload 'perl-completion-mode "perl-completion" nil t)
+(eval-after-load "perl-completion"
+  '(progn
+     (defadvice flymake-start-syntax-check-process (around flymake-start-syntax-check-lib-path activate) (plcmp-with-set-perl5-lib ad-do-it))
+     (define-key plcmp-mode-map (kbd "M-TAB") nil)
+     (define-key plcmp-mode-map (kbd "M-C-o") 'plcmp-cmd-smart-complete)))
+
+;; hook
+(defun my-cperl-mode-hook ()
+  (perl-completion-mode t)
+  (flymake-mode t)
+  (when (boundp 'auto-complete-mode)
+    (defvar ac-source-my-perl-completion
+      '((candidates . plcmp-ac-make-cands)))
+    (add-to-list 'ac-sources 'ac-source-my-perl-completion)))
+(add-hook 'cperl-mode-hook 'my-cperl-mode-hook)
+ 
+;; perl tidy
+;; sudo aptitude install perltidy
+(defun perltidy-region ()
+  "Run perltidy on the current region."
+  (interactive)
+  (save-excursion
+    (shell-command-on-region (point) (mark) "perltidy -q" nil t)))
+(defun perltidy-defun ()
+  "Run perltidy on the current defun."
+  (interactive)
+  (save-excursion (mark-defun)
+                  (perltidy-region)))
+(global-set-key "\C-ct" 'perltidy-region)
+(global-set-key "\C-c\C-t" 'perltidy-defun)
+
+
+
+
+
+;;;;;;;;;;;;;;;;;;
+;;; javascript ;;;
+;;;;;;;;;;;;;;;;;;
+
+;; js2-mode
+; packageでインストールしたいのだが、emacs24以上が必要…
+; なので
+; git clone git://github.com/mooz/js2-mode.git
+; cd js2-mode
+; git checkout emacs23
+; emacs --batch -f batch-byte-compile js2-mode.el
+(autoload 'js2-mode "js2-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+;; (add-hook 'js2-mode-hook
+;;           '(lambda ()
+;;              (setq js2-basic-offs_et 4)))
+;; ;js2-modeが持つインデントの不具合を解決する
+;; (add-hook 'js2-mode-hook 'js-indent-hook)
+
+
+
+
+
+;;;;;;;;;;;;;;;;;;
+;;; emacs-lisp ;;;
+;;;;;;;;;;;;;;;;;;
+
+;; emacs-lisp-modeのフック
+(defun elisp-mode-hooks ()
+  "lisp-mode-hooks"
+  (when (require 'eldoc nil t)
+    (setq eldoc-idle-delay 0.2)
+    (setq eldoc-echo-area-use-multiline-p t)
+    (turn-on-eldoc-mode)))
+(add-hook 'emacs-lisp-mode-hook 'elisp-mode-hooks)
+
+;;; 式の評価結果を注釈するための設定
+; packageでインストール
+(require 'lispxmp)
+;; emacs-lisp-modeでC-c C-dを押すと注釈される
+(define-key emacs-lisp-mode-map (kbd "C-c C-d") 'lispxmp)
+(add-to-list 'ac-modes 'emacs-lisp-mode)
+
+;;; 括弧の対応を保持して編集する設定
+; packageでインストール
+(require 'paredit)
+(add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
+(add-hook 'lisp-mode-hook 'enable-paredit-mode)
+(add-hook 'ielm-mode-hook 'enable-paredit-mode)
+(add-hook 'slime-repl-mode-hook 'enable-paredit-mode)
+
+;;; eldocの設定
+(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
+(setq eldoc-idle-delay 0.2)
+(setq eldoc-minor-mode-string "")
+
+
+
+
+
+;;;;;;;;;;;;;
+;;; slime ;;;
+;;;;;;;;;;;;;
+
+;;; slime(lisp-mode)の設定
+
+(setq inferior-lisp-program "clisp")    ; clisp用
+;;(setq inferior-lisp-program "sbcl")     ; sbcl用
+
+; packageでインストール
+; slime-fancy等のエラーが出たので、package内の全ての.elcを消した
+(require 'slime)
+(slime-setup '(slime-repl slime-fancy slime-banner))
+(setq slime-net-coding-system 'utf-8-unix)
+
+;;lispの補完(auto-completeの拡張版)
+; packageでインストール
+(require 'ac-slime)
+(add-hook 'slime-mode-hook 'set-up-slime-ac)
+(add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
+
+;; HyperSpecを読み込む.
+; wget ftp://ftp.lispworks.com/pub/software_tools/reference/HyperSpec-7-0.tar.gz
+; ~/.emacs.d/doc/hyperspec/に解凍したフォルダとファイルを設置
+; http://d.hatena.ne.jp/khiker/20061231/1167567844
+(setq common-lisp-hyperspec-root
+      (concat "file://" (expand-file-name "~/.emacs.d/doc/hyperspec/HyperSpec/"))
+      common-lisp-hyperspec-symbol-table
+      (expand-file-name "~/.emacs.d/doc/hyperspec/HyperSpec/Data/Map_Sym.txt"))
+;; カーソル付近にある単語の情報を表示
+(slime-autodoc-mode)
+
+;; slime時のポップアップを追加
+;; Apropos
+(push '("*slime-apropos*") popwin:special-display-config)
+;; Macroexpand
+(push '("*slime-macroexpansion*") popwin:special-display-config)
+;; Help
+(push '("*slime-description*") popwin:special-display-config)
+;; Compilation
+(push '("*slime-compilation*" :noselect t) popwin:special-display-config)
+;; Cross-reference
+(push '("*slime-xref*") popwin:special-display-config)
+;; Debugger
+(push '(sldb-mode :stick t) popwin:special-display-config)
+;; REPL
+(push '(slime-repl-mode) popwin:special-display-config)
+;; Connections
+(push '(slime-connection-list-mode) popwin:special-display-config)
+
+
+
+
+;;;;;;;;;;;;
+;;; yaml ;;;
+;;;;;;;;;;;;
+
+;; yaml-mode
+; packgeでインストール
+(when (require 'yaml-mode nil t)
+  (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode)))
+
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; コードテンプレート ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; auto-insert
+(require 'autoinsert)
+; テンプレ格納用ディレクトリ
+(setq auto-insert-directory "~/.emacs.d/template/")
+; ファイル拡張子とテンプレートの対応
+(setq auto-insert-alist
+      (append '(
+                ("\\.pl$" . ["Template.pl"])
+                ("\\.psgi$" . ["Template.psgi"])
+                ("\\.pm$" . ["Template.pm"])
+                ("\\.t$" . ["Template.t"])
+                ) auto-insert-alist))
+(add-hook 'find-file-hooks 'auto-insert)
+
+
+
+;;;;;;;;;;;;
+;;; TAGS ;;;
+;;;;;;;;;;;;
+
+;;; etags ;;;
+
+;; 目的のライブラリのディレクトリに行き、
+;; $ etags *.pm *.pl
+;; で、TAGSファイルを作成
+;; "M-." クラス名や関数名を入力する（デフォルトでカーソル下を取る）
+;; "C-u M-." 複数ヒットする場合、次の候補へ
+;; "M-*" 前の場所に戻る
+
+;;; ctags ;;;
+
+;;; gtags ;;;
