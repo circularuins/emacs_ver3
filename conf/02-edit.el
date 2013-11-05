8c8f4fb7a1ed01aefea7aafbe5f578ad6701f0a6
circularuins <ubuntu11@circularuins.(none)>
2013-11-05 21:55:28 +0900

    org-modeの設定をv23以下に

--- conf/02-edit.el
+++ conf/02-edit.el
@@ -250,20 +250,21 @@
 ;; org-mode
 ;; Emacsでメモ・TODO管理
 (require 'org-install)
-(define-key global-map "\C-cl" 'org-store-link)
-(define-key global-map "\C-ca" 'org-agenda)
-(define-key global-map "\C-cr" 'org-remember)
-(setq org-startup-truncated nil)
-(setq org-return-follows-link t)
-(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
-(org-remember-insinuate)
-(setq org-directory "~/memo/")
-(setq org-default-notes-file (concat org-directory "notes.org"))
-(setq org-agenda-files '("~/memo/notes.org"))
-(setq org-remember-templates
-      '(("Todo" ?t "** TODO %?\n   %i\n   %a\n   %t" nil "Inbox")
-        ("Bug" ?b "** TODO %?   :bug:\n   %i\n   %a\n   %t" nil "Inbox")
-        ("Idea" ?i "** %?\n   %i\n   %a\n   %t" nil "New Ideas")))
+(when (< emacs-major-version 24)
+  (define-key global-map "\C-cl" 'org-store-link)
+  (define-key global-map "\C-ca" 'org-agenda)
+  (define-key global-map "\C-cr" 'org-remember)
+  (setq org-startup-truncated nil)
+  (setq org-return-follows-link t)
+  (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+  (org-remember-insinuate)
+  (setq org-directory "~/memo/")
+  (setq org-default-notes-file (concat org-directory "notes.org"))
+  (setq org-agenda-files '("~/memo/notes.org"))
+  (setq org-remember-templates
+        '(("Todo" ?t "** TODO %?\n   %i\n   %a\n   %t" nil "Inbox")
+          ("Bug" ?b "** TODO %?   :bug:\n   %i\n   %a\n   %t" nil "Inbox")
+          ("Idea" ?i "** %?\n   %i\n   %a\n   %t" nil "New Ideas"))))
 
 ;; 試行錯誤用ファイル
 ; M-x install-elisp-from-emacswiki open-junk-file.el
