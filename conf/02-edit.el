77555fd045e3b6c62318a14671072665bb98ae11
circularuins <ubuntu11@circularuins.(none)>
2013-12-20 14:40:03 +0900

    diredを見やすくするdirexを追加

--- conf/02-edit.el
+++ conf/02-edit.el
@@ -180,6 +180,27 @@
 ;; 現在カーソル位置のファイル・URLを開く
 (ffap-bindings)
 
+;;; direx ;;;
+;; C-u C-x C-j で、通常のdiredを実行
+(defun my/dired-jump ()
+  (interactive)
+  (cond (current-prefix-arg
+         (dired-jump))
+        ((not (one-window-p))
+         (or (ignore-errors
+               (direx-project:jump-to-project-root) t)
+             (direx:jump-to-directory)))
+        (t
+         (or (ignore-errors
+               (direx-project:jump-to-project-root-other-window) t)
+             (direx:jump-to-directory-other-window)))))
+
+(global-set-key (kbd "C-x C-j") 'my/dired-jump)
+
+;; widthは環境に合わせて調整してください。
+(push '(direx:direx-mode :position left :width 40 :dedicated t)
+      popwin:special-display-config)
+
 
 
 
